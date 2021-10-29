cd0401cca0e7a6363eb6842eb4b1e0a955856353
MdSajjadHossin <iamsajjadhossin@gmail.com>
2021-10-29 21:23:29 +0600

    third push

--- fakeData.js
+++ fakeData.js
@@ -1,35 +1,41 @@
 const services = [
     {
+        serviceId: 1,
         name:"Tour Planning",
         serviceImg:"https://image.freepik.com/free-photo/couple-tourists-with-backpacks-mountain_1150-7520.jpg",
         description:"Use Wanderlog to map your journey to figure out the best routes, keep track of hotel and flight bookings and reservations, and read guides from other trip planning websites.",
         cost:'$100'
     },
     {
+        serviceId: 2,
         name:"Tcket Booking",
         serviceImg:"https://image.freepik.com/free-photo/booking-ticket-online-reservation-travel-flight-concept_53876-132645.jpg",
         description:"We Book and Find flight tickets at best airfare at BookMyHoliday.com. Get best discounts and deals on domestic flights booking around the world. ",
         cost:'$80'
     },
     {
+        serviceId: 3,
         name:"Hotel Booking",
         serviceImg:"https://image.freepik.com/free-photo/home-house-exterior-design-showing-tropical-pool-villa-with-sun-bed_41487-564.jpg",
         description:"We Book hotel for your holiday and for your emergency night stay at best hotels near you at BookMyHoliday.com. Get best discounts and deals on hotel booking around the world.",
         cost:'$120'
     },
     {
+        serviceId: 4,
         name:"Table Reservation",
         serviceImg:"https://image.freepik.com/free-photo/reserved-table-restaurant_53876-30324.jpg",
         description:"We reserved table on your favourite hotel and restaurant for your holiday or your emergency meeting at best hotels near you at BookMyHoliday.com. Get best discounts and deals on hotel booking around the world. ",
         cost:'$50'
     },
     {
+        serviceId: 5,
         name:"Spa Reservation",
         serviceImg:"https://image.freepik.com/free-photo/spa-arrangement-with-stones-candle_23-2148290942.jpg",
         description:"We reserved spa for your relaxation on your favourite palces on your need near you at BookMyHoliday.com. Get best discounts and deals on hotel booking around the world.",
         cost:'$70'
     },
     {
+        serviceId: 6,
         name:"All Travelling Support",
         serviceImg:"https://image.freepik.com/free-photo/woman-with-luggage-during-pandemic-airport_23-2148789910.jpg",
         description:"We take care about your sudden or planned tour. We Provide all the services you need on the go. We provide the safest and reliable services when ever or where ever you nedd. Get best discounts and deals on hotel booking around the world.",
